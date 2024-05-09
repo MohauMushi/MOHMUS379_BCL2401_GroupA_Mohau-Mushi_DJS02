c61df10b3692c872e52c9b83c1a69b0d755951b8
Mohau Mushi <david1mushi1@gmail.com>
2024-05-09 15:00:05 +0200

    Change the repeatly if-statements to if-else statement, so that the code don't be too dry

--- scripts.js
+++ scripts.js
@@ -7,33 +7,26 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
 
-  // Performing the division
-  const decimalRemoveValue = dividend / divider;
-  
-  // Display the result without decimal 
-  result.innerText = Math.floor(decimalRemoveValue);
 
-  // Checking if both inputs are empty or missing
-  if(!dividend || !divider){
-    result.innerText = `Division not performed. Both values are required in inputs. Try again`;
-    return;
-  };
-  // Checking if both inputs are numbers
-  if(isNaN(dividend) || isNaN(divider)){
-    // Crashing the program
-    document.body.classList.add(`critical-error`);
-    document.body.innerText = `Something critical went wrong. Please reload the page`;
-    console.error(`Invalid inputs`, new Error(`Invalid input`));
-    return;
-  };
-  
-  // Checking for the invalid divisions
-  if(divider === '0'){
-    result.classList.add(`error-message`);
-    result.innerText = `Division not performed. Invalid number provided. Try again`;
-    console.error(`Invalid devision`, new Error(`Division by Zero`));
-    return;
-  }
+  // Performing the division
+const decimalRemoveValue = dividend / divider;
+
+// Checking if both inputs are empty or missing
+if (!dividend || !divider) {
+  result.innerText = `Division not performed. Both values are required in inputs. Try again`;
+} else if (isNaN(dividend) || isNaN(divider)) {// Checking if both inputs are numbers
+  // Crashing the program
+  document.body.classList.add(`critical-error`);
+  document.body.innerText = `Something critical went wrong. Please reload the page`;
+  console.error(`Invalid inputs`, new Error(`Invalid input`));
+} else if (divider === '0') { // Checking for the invalid divisions
+  result.classList.add(`error-message`);
+  result.innerText = `Division not performed. Invalid number provided. Try again`;
+  console.error(`Invalid devision`, new Error(`Division by Zero`));
+} else {
+  // Display the result without decimal
+  result.innerText = Math.floor(decimalRemoveValue);
+}
 
   
 });
